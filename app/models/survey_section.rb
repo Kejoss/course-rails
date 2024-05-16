8ad91a4db8a87a3fd7b48a81f3f23c7d4f603edf
Kejoss <kevinanderson22082001@gmail.com>
2024-05-16 18:18:27 +0200

    Video81

--- app/models/survey_section.rb
+++ app/models/survey_section.rb
@@ -2,14 +2,21 @@ class SurveySection < ApplicationRecord
   belongs_to :survey
   belongs_to :section
 
+  before_create :configure_order
+
   private
 
   def self.check_no_repeat_records(survey, ids, new_ids)
     survey_sections = where(survey_id: survey.id)
-
     ids.each{ |id| new_ids << id.to_i unless survey_sections.find_by(section_id: id).present? }
 
 
     new_ids
+  end
+
+  def configure_order
+    surv_sec = SurveySection.where(survey_id: survey.id).order(order: :asc)
+
+    surv_sec.empty? ? self.order = 1 : self.order = surv_sec.last.order + 1
   end
 end
