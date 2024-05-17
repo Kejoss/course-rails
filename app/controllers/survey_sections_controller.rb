50d3f1232bb28df606c38a53c82012048160f8b9
Kejoss <kevinanderson22082001@gmail.com>
2024-05-17 11:18:01 +0200

    Video83

--- app/controllers/survey_sections_controller.rb
+++ app/controllers/survey_sections_controller.rb
@@ -1,5 +1,5 @@
 class SurveySectionsController < ApplicationController
-    before_action :set_survey, only: %i[add_sections save_sections delete_section]
+    before_action :set_survey, only: %i[add_sections save_sections delete_section up_section]
 
     def add_sections
     end
@@ -21,13 +21,25 @@ class SurveySectionsController < ApplicationController
     end
 
     def delete_section
-      @survey_section = SurveySection.where(survey_id: @survey.id).find_by(section_id: params[:id])
-      @survey_section.destroy
-      SurveySection.reorder_objects(@survey.id)  
+        @survey_section = SurveySection.where(survey_id: @survey.id).find_by(section_id: params[:id])
+        @survey_section.destroy
+        SurveySection.reorder_objects(@survey.id)  
 
-      respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('sections', partial: 'survey_sections/survey_sections', locals: { survey: Survey.find(@survey.id) }) }
-      end
+        respond_to do |format|
+            format.turbo_stream { render turbo_stream: turbo_stream.replace('sections', partial: 'survey_sections/survey_sections', locals: { survey: Survey.find(@survey.id) }) }
+        end
+    end
+
+    def up_section
+        @survey_section = SurveySection.where(survey_id: @survey.id).find_by(section_id: params[:id])
+        @prev_surv_sec = SurveySection.where(survey_id: @survey.id).find_by(order: @survey_section.order - 1)
+        @survey_section.update!(order: @survey_section.order - 1)
+        @prev_surv_sec.update!(order: @prev_surv_sec.order + 1)
+
+        respond_to do |format|
+            format.turbo_stream { render turbo_stream: turbo_stream.replace('sections', partial: 'survey_sections/survey_sections', locals: { survey: Survey.find(@survey.id) }) }
+        end
+
     end
 
     private 
